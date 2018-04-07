50cd4cd8b9a3325a58bf5e363468a77aaffb905b
Deni Marswandi <denimarswandi@gmail.com>
2018-04-07 08:55:41 +0800

    change table

--- app/models/models.py
+++ app/models/models.py
@@ -1,6 +1,6 @@
 from app import db
 
-class Ayam(db.Model):
+class Mahasiswa(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    jenis = db.Column(db.String(50), nullable=False)
-    kelamin = db.Column(db.String(1), nullable=False)
+    nim = db.Column(db.String(50), nullable=False)
+    nama = db.Column(db.String(1), nullable=False)
